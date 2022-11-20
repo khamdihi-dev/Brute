b309006da94318bbfd0d415d3e080f26da688915
khamdihi-dev <118111949+khamdihi-dev@users.noreply.github.com>
2022-11-20 19:35:48 +0700

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from os import system
-from run import folder
-
-system('git pull') ; system('clear')
-if __name__ == '__main__':
-        try:
-                folder()
-        except Exception as e:exit(str(e))
